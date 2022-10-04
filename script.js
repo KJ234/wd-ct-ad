4b86674600bf94cfbddd7d8b1f60a0d70afcae9b
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-10-04 19:59:09 +0100

    starting

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-
 //document.getelemntbyid or query selector
+let formInput = document.querySelector(".input");
+let searchButton = document.querySelector(".searchButton");
+// api key
+let APIKEY = "bd0d0c0b9ffe1286395e4abb56d49400";
 
-
-// api key 
-
-
-// fetch request
-//.then res
-
-//catch error
-
-
+function displaySpecificForecast() {
+  // fetch request
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      formInput.value +
+      "&appid=" +
+      APIKEY
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 // if statments
-
-
-
 
 //.add eventlistener
 
-
 //pressclick
+
+searchButton.addEventListener("click", displaySpecificForecast);
